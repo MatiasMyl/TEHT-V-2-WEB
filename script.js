e3e61a33b8cb01f337e1eb536231120b0077bde9
MatiasMyl <matias.myllyharju@student.hamk.fi>
2026-02-20 21:21:13 +0200

    update

--- script.js
+++ script.js
@@ -7,8 +7,29 @@ async function getData() {
     }
 
     const result = await response.json();
-    console.log(result);
+    return result;
   } catch (error) {
     console.error(error.message);
   }
 }
+
+
+getData().then(result => {
+  console.log(result);
+  //answerMainColour = result.answer1[1].option
+  //answerPopulation = result.answer2[1].option
+  //answerCelebrated = result.answer3[0].option
+
+  document.getElementById("label11").innerHTML = result.answer3[0].option
+});
+
+
+function onClick(){
+  wrongAnswers = [];
+  correctAnswers = [];
+
+
+  
+}
+
+
