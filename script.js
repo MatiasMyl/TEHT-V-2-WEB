2add0eed52209d9642f7959a70d918aff942f9b4
MatiasMyl <matias.myllyharju@student.hamk.fi>
2026-02-20 22:49:41 +0200

    update

--- script.js
+++ script.js
@@ -16,11 +16,13 @@ async function getData() {
 
 getData().then(result => {
   console.log(result);
-  //answerMainColour = result.answer1[1].option
-  //answerPopulation = result.answer2[1].option
-  //answerCelebrated = result.answer3[0].option
+
+  //Correct answers
+  answerMainColour = result.answer1[1].option //White
+  answerPopulation = result.answer2[1].option //5.5 million
+  answerCelebrated = result.answer3[0].option //Independence day
   
-  //Question 2 set up
+  //Question 1 set up
   document.getElementById("p1").innerHTML = result.question1[0].option
   document.getElementById("p2").innerHTML = result.question1[1].option
   document.getElementById("label1").innerHTML = result.answer1[0].option
@@ -33,6 +35,7 @@ getData().then(result => {
   document.getElementById("label4").innerHTML = result.answer2[0].option
   document.getElementById("label5").innerHTML = result.answer2[1].option
   document.getElementById("label6").innerHTML = result.answer2[2].option
+  
   //Question 3 set up
   document.getElementById("p5").innerHTML = result.question3[0].option
   document.getElementById("p6").innerHTML = result.question3[1].option
@@ -42,11 +45,30 @@ getData().then(result => {
 });
 
 
+//Button functionality
+
 function onClick(){
-  wrongAnswers = [];
-  correctAnswers = [];
 
+ let wrongAnswers = 0;
+ let correctAnswers = 0;
 
+  if(document.getElementById("white").checked){
+    correctAnswers ++;
+  }else{
+    wrongAnswers ++;
+  }
+  if(document.getElementById("5.5mil").checked){
+  correctAnswers ++;
+  }else{
+    wrongAnswers ++;
+  }
+  if(document.getElementById("independence").checked){
+  correctAnswers ++;
+  }else{
+    wrongAnswers ++;
+  }
+  
+  document.getElementById("results").innerHTML = "You scored " + correctAnswers + "/3" 
   
 }
 
