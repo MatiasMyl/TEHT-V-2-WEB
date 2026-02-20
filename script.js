3082b86cc5b73ca66a3a5f9d607dc3b0302ec449
MatiasMyl <matias.myllyharju@student.hamk.fi>
2026-02-20 21:49:23 +0200

    update

--- script.js
+++ script.js
@@ -19,8 +19,15 @@ getData().then(result => {
   //answerMainColour = result.answer1[1].option
   //answerPopulation = result.answer2[1].option
   //answerCelebrated = result.answer3[0].option
+  
+  document.getElementById("p3").innerHTML = result.question2[0].option
+  document.getElementById("p4").innerHTML = result.question2[1].option
+  document.getElementById("label4").innerHTML = result.answer2[0].option
+  document.getElementById("label5").innerHTML = result.answer2[1].option
+  document.getElementById("label6").innerHTML = result.answer2[2].option
+  //Question 3 set up
   document.getElementById("p5").innerHTML = result.question3[0].option
-  document.getElementById("p6").innerHTML = result.question3[0].option
+  document.getElementById("p6").innerHTML = result.question3[1].option
   document.getElementById("label7").innerHTML = result.answer3[0].option
   document.getElementById("label8").innerHTML = result.answer3[1].option
   document.getElementById("label9").innerHTML = result.answer3[2].option
